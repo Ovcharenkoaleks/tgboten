70231dd0675961a869d48824b7a3dfc739272507
unknown <ovcharenkoaleksov@gmail.com>
2023-05-12 15:48:57 +0300

    step1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Telegraf = require('telegraf')
 const fetch = require('node-fetch')
-
 const bot = new Telegraf('<5616726218:AAERTLFsPKOPEnZQmm_BHStGpZlFtk_aD6Q>')
 
 bot.start((ctx) => ctx.reply('Welcome!'))
