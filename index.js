9afe91e3ab7218cc1c724505ad65c5655a682a71
unknown <ovcharenkoaleksov@gmail.com>
2023-05-12 16:04:20 +0300

    tgbot2

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const { Telegraf } = require('telegraf')
+const Telegraf = require('telegraf');
+const fetch = require('node-fetch').default;
 
-const bot = new Telegraf('5616726218:AAERTLFsPKOPEnZQmm_BHStGpZlFtk_aD6Q')
+const bot = new Telegraf('5616726218:AAERTLFsPKOPEnZQmm_BHStGpZlFtk_aD6Q');
 
-bot.start((ctx) => ctx.reply('Hello, I am your English learning bot!'))
+bot.start((ctx) => ctx.reply('Hello, I am your English learning bot!'));
 
-bot.launch()
+bot.on('text', async (ctx) => {
+  const text = ctx.message.text;
+  const translations = await getTranslations(text);
+  const quizOptions = {
+    question: `${text} - как переводится на английский?`,
+    options: translations,
+    correct_option_id: 0,
+    explanation: `Перевод слова "${text}" на английский язык: ${translations[0]}`,
+  };
+  ctx.replyWithQuiz(quizOptions);
+});
+
+async function getTranslations(text) {
+  const response = await fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=ru|en`);
+  const json = await response.json();
+  const translations = [];
+
+  if (json.responseData && json.responseData.translatedText) {
+   
